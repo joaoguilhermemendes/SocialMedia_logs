d23994c53d490b6d6a60a2f4fe9c1e863dfb8a39
joaoguilhermemendes <joaoguilhermemendes@id.uff.br>
2024-06-10 22:30:49 -0300

    first commit, defining the concept

---
+++ logs.py
@@ -0,0 +1,42 @@
+# User wills select a social network (e.g. "Instagram")
+# User wills enter a target username (e.g. "@joao_guilherme26")
+
+# Using selenium and beautifulsoup, the program wills list all dates, descriptions, places...
+# Maybe followers, average of likes, bio, full name...
+
+#############################################################################################
+
+# user: @joao_guilherme26
+# name: João Guilherme Mendes
+# bio: 📚 ⌨️ ⛱️ - 022 -💻 UFF - Ciência da Computação
+
+# publications: 19
+# followers: 280
+# following: 377
+
+# Select the index number for more info (likes, comments)
+# [ 1 ] 16.12.2023 - Arraial do Cabo - no description
+# [ 2 ] 21.11.2022 - Arraial do Cabo - no description
+# [ 3 ] 24.10.2022 - Lumiar - ⚡
+# [ 4 ] 10.09.2022 - São Pedro da Aldeia - 2.0
+# ...
+
+#############################################################################################
+
+# Selected option "4"
+# 10.09.2022 - São Pedro da Aldeia - 2.0
+# link da image: https://instagram.fgig14-2.fna.fbcdn.net/v/t51.29350-15/306410454_163644529590679_6785695299816030176_n.webp?stp=dst-jpg_e35&efg=eyJ2ZW5jb2RlX3RhZyI6ImltYWdlX3VybGdlbi4xMDgweDEzNTAuc2RyLmYyOTM1MCJ9&_nc_ht=instagram.fgig14-2.fna.fbcdn.net&_nc_cat=107&_nc_ohc=PBtxNbrcGCMQ7kNvgGXI0Pu&edm=AEhyXUkBAAAA&ccb=7-5&ig_cache_key=MjkyNDM3NjUyMjE1NzMxMDc4MA%3D%3D.2-ccb7-5&oh=00_AYDps94BytA5fpaLfqwVq8Qr-sfziqQee1jVqhWjQ_PJ5Q&oe=666B763C&_nc_sid=cf751b
+# link do post: https://www.instagram.com/p/CiVePPwrcM8/
+
+# 10 comments:
+
+# @joaomauricio3007: 
+#    -> "Cadê a polícia?"
+
+# @leonardo_moura_martins:
+#    -> "Se me chamar eu vou hj hein..."
+#           @joao_guilherme26:
+#               -> "Só vem"
+# ...
+
+#############################################################################################
