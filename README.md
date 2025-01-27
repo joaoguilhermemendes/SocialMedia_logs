3603cbada1c91e526a61990e45c51069f44314fa
joaoguilhermemendes <jmgvh9000@gmail.com>
2025-01-27 13:49:48 -0300

    Scrapping profile/posts content - no download, comments limited

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
-![alt text](images/False.png)
+# SocialMedia Logs
+
+This project is designed to manage and analyze instagram logs. It provides tools to collect, process, and visualize data from instagram.
+
+
+## Installation
+
+1. Clone the repository:
+    ```sh
+    git clone https://github.com/yourusername/SocialMedia_logs.git
+    ```
+2. Navigate to the project directory:
+    ```sh
+    cd SocialMedia_logs
+    ```
+3. Install the required dependencies:
+    ```sh
+    pip install -r requirements.txt
+    ```
+
+## Usage
+
+1. Collect data:
+    ```sh
+    python logs.py
+    ```
+
+## Demo
+
+![Demo](demo.png)
+
+## Current Version and Future Features
+
+The current version of the project supports data collection, processing, and visualization exclusively for Instagram. 
+
+In future updates, we plan to expand support to include additional social media platforms. However, this expansion is not expected to happen in the immediate future.
+
+## Features
+- [ ] **Data Collection**: Gather logs from multiple social media platforms.
+- [ ] **Data Processing**: Clean and preprocess the collected data.
+- [ ] **Data Visualization**: Generate insightful visualizations from the processed data.
+
+
+## Contributing
+
+Contributions are welcome! Please open an issue or submit a pull request for any changes.
+
+## License
+
+This project is licensed under the MIT License. See the [LICENSE](LICENSE) file for details.
+
--- logs.py
+++ logs.py
@@ -1,33 +1,84 @@
-import pandas as pd
-import time
+from colorama import Fore, Style
+import instaloader
 
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.wait import WebDriverWait
+print(Style.BRIGHT + Fore.GREEN + " ")
+print("   _____            _       ____  ___         ___            __                ")
+print("  / ___/____  _____(_)___ _/ /  |/  /__  ____/ (_)___ _     / /___  ____ ______")
+print("  \__ \/ __ \/ ___/ / __ `/ / /|_/ / _ \/ __  / / __ `/    / / __ \/ __ `/ ___/")
+print(" ___/ / /_/ / /__/ / /_/ / / /  / /  __/ /_/ / / /_/ /    / / /_/ / /_/ (__  ) ")
+print("/____/\____/\___/_/\__,_/_/_/  /_/\___/\__,_/_/\__,_/____/_/\____/\__, /____/ ")
+print(Fore.YELLOW + "  | - >" + Fore.WHITE + "  version 1.0" + Fore.GREEN + "                               /_____/       /____/")
+print(Fore.YELLOW + "  | - - >" + Fore.GREEN + "  CATCH ALL USER LOGS INTO SOME SOCIAL MEDIA")
+print(Fore.YELLOW + "  | - - - >" + Fore.WHITE + "  www.github.com/joaoguilhermemendes/SociaMedia_logs")
+print(" ")
+print(" ")
+print(Fore.YELLOW + "[ 1 ]" + Fore.WHITE + " INSTAGRAM")
+print(Style.DIM + "[ 2 ]" + Fore.WHITE + " TWITTER")
+print(Style.DIM + "[ 3 ]" + Fore.WHITE + " FACEBOOK")
+print(Style.RESET_ALL + " " + Style.BRIGHT)
 
-service = Service() # Initialize a Chrome web driver instance
-options = webdriver.ChromeOptions() # Select Chrome as the default browser
-driver = webdriver.Chrome(service=service, options=options) # Start the WebDriver with 'options' and 'service' defined
+social_media = int(input(Fore.YELLOW + "Select the social media: (e.g. 1) - " + Fore.WHITE))
+user = str(input(Fore.YELLOW + "Username: (e.g. username) - " + Fore.WHITE))
 
-url = 'https://www.instagram.com/'
-driver.get(url)
+print("-----------------------------------------------------------------------------")
+print(Style.RESET_ALL + " " + Style.BRIGHT)
 
-username = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='username']")))
-password = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='password']")))
+loader = instaloader.Instaloader() # Create an instance of Instaloader class
 
-username.clear()
-password.clear()
-username.send_keys("xxxxxxxxxxx")
-password.send_keys("xxxxxxxxxxxxxxx")
+profile = instaloader.Profile.from_username(loader.context, user) # Get the profile of the user
+full_name = profile.full_name
+bio = profile.biography
+followers = profile.followers
+followees = profile.followees
+posts = profile.mediacount
 
-login = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[type='submit']"))).click()
+print(Fore.GREEN + "User: " + Fore.WHITE + f"{user}")
+print(Fore.GREEN + "Name: " + Fore.WHITE + f"{full_name}")
+print(Fore.GREEN + "Bio: " + Fore.WHITE)
+print(f"{bio}")
 
-time.sleep(5)
-alert = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[type='button']"))).click()
-alert2 = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[class='_a9-- _ap36 _a9_1']"))).click()
+print(" ")
+print(" ")
 
+print(Fore.GREEN + "Followers: " + Fore.WHITE + f"{followers}")
+print(Fore.GREEN + "Following: " + Fore.WHITE + f"{followees}")
+print(Fore.GREEN + "Posts: " + Fore.WHITE + f"{posts}")
+posts = []
+print(Fore.YELLOW + "|") 
+for index, post in enumerate(profile.get_posts(), 1): # Get the posts of the user
+    posts.append(post)
+    print(Fore.YELLOW + "| - - - >" + Fore.GREEN + f" [{index}]" + Fore.WHITE + f" {str(post.date)[:10]}")
 
-input("Press any key to close browser... ")
+print(" ")
+print(" ")
+
+more_info = str(input("More info (Y/N)? ")).upper()
+while more_info == 'Y':    
+    post_index = int(input("Select the log index number: "))
+
+    print("-----------------------------------------------------------------------------")
+    print(" ")
+
+    print(Fore.GREEN + "DESCRIPTION: " + Fore.WHITE + f"{posts[post_index-1].caption}")
+    print(" ")
+    print(Fore.GREEN + "POST: " + Fore.WHITE + f"{post_index}")
+    print(Fore.GREEN + "LIKES: " + Fore.WHITE + f"{posts[post_index-1].likes}")
+    print(Fore.GREEN + "COMMENTS: " + Fore.WHITE + f"{posts[post_index-1].comments}")
+    print("|")
+
+    loader.login('usename', 'password') # Login with your user and password
+    for comment in posts[post_index-1].get_comments(): # Get the comments of the selected post
+        print("| - - > " + Fore.RED + f"@{comment.owner.username}: " + Fore.WHITE + f"{comment.text}")
+
+    print(" ")
+    print("-----------------------------------------------------------------------------")
+    print(" ")
+    more_info = str(input("More info (Y/N)? ")).upper()
+
+print(" ")
+print(" ")
+print(" ")
+print(Fore.YELLOW + "----------------------------------------- END ------------------------------------")
+print(" ")
+print(" ")
+print(" ")
